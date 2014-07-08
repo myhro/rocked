527531e1107ee5919340a157fdd4695e62c6603e
Tiago Ilieve <tiago.myhro@gmail.com>
2014-07-08 14:46:27 -0300

    Refactored in an object-oriented way

--- rocked.py
+++ rocked.py
@@ -7,90 +7,117 @@ import docopt
 import yaml
 
 
-cli = '''Rocked, a thin wrapper to manage Docker containers.
+class Rocked:
+    def __init__(self, cli):
+        self.args = docopt.docopt(cli.strip())
+        self.container = None
+        self.show_status = False
 
-Usage:
-    rocked build <file.yml>
-    rocked kill <file.yml>
-    rocked logs <file.yml>
-    rocked run <file.yml> [--] [<command>...]
-    rocked start <file.yml>
-    rocked status <file.yml>
-    rocked stop <file.yml>
-    rocked -h | --help
+        if os.path.exists(self.args['<file.yml>']):
+            yml_file = self.args['<file.yml>']
+        else:
+            yml_file = os.path.join(os.getcwd(), self.args['<file.yml>'])
+        self.manifest = yaml.load(os.path.expandvars(open(yml_file).read()))
 
-Options:
-    -h --help   Show this screen.
-'''
+        if self.args['status']:
+            self.show_status = True
 
-args = docopt.docopt(cli)
+        self.get_container()
 
-if os.path.exists(args['<file.yml>']):
-    yml_file = args['<file.yml>']
-else:
-    yml_file = os.path.join(os.getcwd(), args['<file.yml>'])
+        if self.args['build']:
+            self.build()
+        elif self.args['kill']:
+            self.kill()
+        elif self.args['logs']:
+            self.logs()
+        elif self.args['run'] or self.args['start']:
+            self.run()
+        elif self.args['status']:
+            self.status()
+        elif self.args['stop']:
+            self.stop()
 
-manifest = yaml.load(os.path.expandvars(open(yml_file).read()))
+    def build(self):
+        subprocess.call(['docker', 'build', '-t', self.manifest['image'], manifest['build']])
 
-if args['build']:
-    subprocess.call(['docker', 'build', '-t', manifest['image'], manifest['build']])
-    exit(0)
+    def get_container(self):
+        ps_command = ['docker', 'ps']
+        if not self.show_status:
+            ps_command.extend(['-a'])
+        container_list = subprocess.check_output(ps_command).strip().split('\n')
+        if len(container_list) > 1:
+            for line in [c.split() for c in container_list[1:]]:
+                container_id = line[0]
+                inspect = json.loads(subprocess.check_output(['docker', 'inspect', container_id])[1:-1])
+                if inspect['Name'] == u'/{name}'.format(name=self.manifest['name']):
+                    self.container = container_id
+                    break
 
-ps_command = ['docker', 'ps']
-show_status = False
-if args['status'] or not any([args['build'], args['kill'], args['logs'], args['run'], args['start'], args['stop']]):
-    show_status = True
-else:
-    ps_command.extend(['-a'])
+    def kill(self):
+        if self.container:
+            subprocess.call(['docker', 'kill', self.container])
 
-container = None
-container_list = subprocess.check_output(ps_command).strip().split('\n')
-if len(container_list) > 1:
-    for line in [c.split() for c in container_list[1:]]:
-        container_id = line[0]
-        inspect = json.loads(subprocess.check_output(['docker', 'inspect', container_id])[1:-1])
-        if inspect['Name'] == u'/{name}'.format(name=manifest['name']):
-            container = container_id
-            break
+    def logs(self):
+        if self.container:
+            subprocess.call(['docker', 'logs', self.container])
 
-if show_status:
-    if container:
-        print manifest['name'] + ': running'
-    else:
-        print manifest['name'] + ': not running'
+    def stop(self):
+        if self.container:
+            subprocess.call(['docker', 'stop', self.container])
 
-if container:
-    if args['kill']:
-        subprocess.call(['docker', 'kill', container])
-    if args['logs']:
-        subprocess.call(['docker', 'logs', container])
-    if args['stop']:
-        subprocess.call(['docker', 'stop', container])
+    def status(self):
+        if self.container:
+            print self.manifest['name'] + ': running'
+        else:
+            print self.manifest['name'] + ': not running'
 
-if args['start'] or args['run']:
-    run_command = ['docker', 'run']
-    if not args['run']:
-        if container:
-            subprocess.call(['docker', 'rm', container])
-        run_command.extend(['-d'])
-        run_command.extend(['--name', manifest['name']])
-    else:
-        run_command.extend(['--rm', '-t', '-i'])
-    if 'environment' in manifest:
-        for e in manifest['environment']:
-            run_command.extend(['-e', e])
-    if 'links' in manifest:
-        for l in manifest['links']:
-            run_command.extend(['--link', l])
-    if 'ports' in manifest and not args['run']:
-        for p in manifest['ports']:
-            run_command.extend(['-p', p])
-    if 'volumes' in manifest:
-        for v in manifest['volumes']:
-            run_command.extend(['-v', v])
-    run_command.extend([manifest['image']])
-    if args['<command>']:
-        run_command.extend(args['<command>'])
-    elif 'command' in manifest:
-        run_command.extend(manifest['command'].split())
-    subprocess.call(run_command)
+    def run(self):
+        run_command = ['docker', 'run']
+        if self.args['run']:
+            run_command.extend(['--rm', '-t', '-i'])
+        else:
+            if self.container:
+                subprocess.call(['docker', 'rm', self.container])
+            run_command.extend(['-d'])
+            run_command.extend(['--name', self.manifest['name']])
+        if 'environment' in self.manifest:
+            for e in self.manifest['environment']:
+                run_command.extend(['-e', e])
+        if 'links' in self.manifest:
+            for l in self.manifest['links']:
+                run_command.extend(['--link', l])
+        if 'ports' in self.manifest and not self.args['run']:
+            for p in self.manifest['ports']:
+                run_command.extend(['-p', p])
+        if 'volumes' in self.manifest:
+            for v in self.manifest['volumes']:
+                run_command.extend(['-v', v])
+        run_command.extend([self.manifest['image']])
+        if self.args['<command>']:
+            run_command.extend(self.args['<command>'])
+        elif 'command' in self.manifest:
+            run_command.extend(self.manifest['command'].split())
+        subprocess.call(run_command)
+
+
+def main():
+    cli = '''
+    Rocked, a thin wrapper to manage Docker containers.
+
+    Usage:
+        rocked build <file.yml>
+        rocked kill <file.yml>
+        rocked logs <file.yml>
+        rocked run <file.yml> [--] [<command>...]
+        rocked start <file.yml>
+        rocked status <file.yml>
+        rocked stop <file.yml>
+        rocked -h | --help
+
+    Options:
+        -h --help   Show this screen.
+    '''
+    rocked = Rocked(cli)
+
+if __name__ == '__main__':
+    main()
